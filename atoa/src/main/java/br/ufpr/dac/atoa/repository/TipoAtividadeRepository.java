44d456bd6990634c88deb3d7e8588740e31b3ed5
giovannibeltrame <gio.beltrame@gmail.com>
2017-07-21 20:26:54 -0300

    Clean

--- atoa/src/main/java/br/ufpr/dac/atoa/repository/TipoAtividadeRepository.java
+++
@@ -1,9 +0,0 @@
-package br.ufpr.dac.atoa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.ufpr.dac.atoa.entity.TipoAtividade;
-
-public interface TipoAtividadeRepository extends JpaRepository<TipoAtividade, Long> {
-
-}
